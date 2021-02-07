57b4b05abfc672f893163fa25458cc004b163e57
Milo Weinberg <iapetus011@gmail.com>
2021-02-06 20:17:13 -0500

    fix import

--- src/je/net/packets/handshake.nim
+++ src/je/net/packets/handshake.nim
@@ -1,6 +1,6 @@
-import src/je/types/packet/Packet
+import src/je/types/packet
 
 type
-  HandshakeHandshake* = ref object of Packet
+  HandshakeHandshake* = ref object of packet.Packet
     const id: int = 0
     const to: int = 0

8c82f6bffe74f2b23467403bab1ded3cbb8e9306
Milo Weinberg <iapetus011@gmail.com>
2021-02-07 00:27:43 -0500

    work on handshake.nim

--- src/je/net/packets/handshake.nim
+++ src/je/net/packets/handshake.nim
@@ -1,6 +1,16 @@
-import src/je/types/packet
+from streams import Stream
+
+from ../../types/packet import Packet
 
 type
-  HandshakeHandshake* = ref object of packet.Packet
-    const id: int = 0
-    const to: int = 0
+  HandshakeHandshake* = ref object of Packet
+    protocol_version*: int16
+    server_address*: string
+    server_port*: int16
+    next_state*: byte
+
+proc new_HandshakeHandshake(v: int16, s: string, p: int16, n: byte): HandshakeHandshake =
+  return HandshakeHandshake(protocol_version: v, server_address: s, server_port: p, next_state: n)
+
+proc unpack_handshake(s: Stream): HandshakeHandshake =
+  return new_HandshakeHandshake()

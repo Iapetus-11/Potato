4d4ac22e60db68cd9ba4de687e888a6c75290302
Milo Weinberg <iapetus011@gmail.com>
2021-02-07 22:52:42 -0500

    update pack overloading

--- src/je/util/struct.nim
+++ src/je/util/struct.nim
@@ -1,12 +1,13 @@
 import endians
+import streams
 
 const formatCodes: array[0..10, char] = ['b', 'B', '?', 'h', 'H', 'i', 'I', 'q', 'Q', 'f', 'd']
 const endianCodes: array[0..1, char] = ['>', '<']
 
 type
-  anyStructData = byte |  uint8 | bool | int16 | uint16 | int32 | uint32 | int64 | uint64 | float
+  anyStructData = int8 | uint8 | bool | int16 | uint16 | int32 | uint32 | int64 | uint64 | float
 
-proc pack(b: byte, endian: char): byte =  # endianness doesn't matter here since it's just one byte
+proc pack(b: int8, endian: char): int8 =  # endianness doesn't matter here since it's just one byte
   return b
 
 proc pack(b: uint8, endian: char): uint8 = # endianness again doesn't matter here since it's just one byte
@@ -63,6 +64,12 @@ proc pack(q: uint64, endian: char): uint64 =
   else:
     littleEndian64(addr result, addr q)
 
+# proc pack(f: float, endian: char): float =
+#   var f: float = f
+#
+#   if endian == '>':
+#     bigEndianFloat
+
 proc pack(fmt: seq[char], data: seq[anyStructData]): string =
   if len(fmt) != len(data) + 1:
     raise newException(ValueError, "Data is invalid for format given.")
@@ -70,5 +77,9 @@ proc pack(fmt: seq[char], data: seq[anyStructData]): string =
   if not fmt[0] in endianCodes:
     raise newException(ValueError, "Invalid endianness specified in format.")
 
+  var s = StringStream()
+
   for i in countup(0, len(data)):
-    return ""
+    s.write(pack(data[i], fmt[0]))
+
+  return s

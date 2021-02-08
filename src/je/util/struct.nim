c64d4d98b344ba5f3dc2107a059b2ce2c7fec19f
Milo Weinberg <iapetus011@gmail.com>
2021-02-07 22:44:38 -0500

    add pack()s for bytes, bools, shorts, and ints (signed + unsigned)

--- src/je/util/struct.nim
+++ src/je/util/struct.nim
@@ -1,3 +1,4 @@
+import endians
 
 const formatCodes: array[0..10, char] = ['b', 'B', '?', 'h', 'H', 'i', 'I', 'q', 'Q', 'f', 'd']
 const endianCodes: array[0..1, char] = ['>', '<']
@@ -5,9 +6,46 @@ const endianCodes: array[0..1, char] = ['>', '<']
 type
   anyStructData = byte |  uint8 | bool | int16 | uint16 | int32 | uint32 | int64 | uint64 | float
 
-proc pack(b: byte, endian: char): string =
-  if endian == '>':  # big
-    return ""
+proc pack(b: byte, endian: char): byte =  # endianness doesn't matter here since it's just one byte
+  return b
+
+proc pack(b: uint8, endian: char): uint8 = # endianness again doesn't matter here since it's just one byte
+  return b
+
+proc pack(b: bool, endian: char): bool = # big woop endianness doesn't matter bc bool is just one byte
+  return b
+
+proc pack(h: int16, endian: char): int16 =
+  var h: int16 = h
+
+  if endian == '>':
+    bigEndian16(addr result, addr h)
+  else:
+    littleEndian16(addr result, addr h)
+
+proc pack(h: uint16, endian: char): uint16 =
+  var h: uint16 = h
+
+  if endian == '>':
+    bigEndian16(addr result, addr h)
+  else:
+    littleEndian16(addr result, addr h)
+
+proc pack(i: int32, endian: char): int32 =
+  var i: int32 = i
+
+  if endian == '>':
+    bigEndian32(addr result, addr i)
+  else:
+    littleEndian32(addr result, addr i)
+
+proc pack(i: uint32, endian: char): uint32 =
+  var i: uint32 = i
+
+  if endian == '>':
+    bigEndian32(addr result, addr i)
+  else:
+    littleEndian32(addr result, addr i)
 
 proc pack(fmt: seq[char], data: seq[anyStructData]): string =
   if len(fmt) != len(data) + 1:

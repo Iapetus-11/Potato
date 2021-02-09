0e9065513792bb8c681a9b352115bf0af0d85058
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 16:01:32 -0500

    name clutter

--- nimcraft.nim
+++ nimcraft.nim
@@ -2,6 +2,6 @@ import src/je/util/struct
 
 var data: array[0..3, int] = [1, 2, 100000, 6969]
 
-var result: string = pack(">i?if", data)
+var result: string = struct.pack(">i?if", data)
 
 echo repr(result)

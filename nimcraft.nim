a9cbf3400633373ee62927fa3fb9cf12367c7188
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 17:51:42 -0500

    update example

--- nimcraft.nim
+++ nimcraft.nim
@@ -9,4 +9,7 @@ struct.pack(s, '<', 'i', 69)
 struct.pack(s, '>', 'f', 12.1)
 struct.pack(s, '>', 'q', 100000000)
 
-echo repr(s.readAll())
+echo struct.unpackUint8(s)
+echo struct.unpackInt32(s, '<')
+echo struct.unpackFloat32(s)
+echo struct.unpackInt64(s)

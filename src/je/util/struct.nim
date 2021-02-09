d0c0e9cc766b1bb15ac04e14814faeee5251db1c
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 16:05:35 -0500

    add second pack method for packing into a preexisting stream

--- src/je/util/struct.nim
+++ src/je/util/struct.nim
@@ -93,3 +93,40 @@ proc pack*(fmt: string, data: openArray[int]): string =
 
   s.setPosition(0)
   return s.readAll()
+
+proc pack*(s: Stream, fmt: string, data: openArray[int]) {.discardable.} =
+  if len(fmt) != len(data) + 1:
+    raise newException(ValueError, "Data is invalid for format given.")
+
+  let endianness: char = fmt[0]
+
+  if endianness != '>' and endianness != '<':
+    raise newException(ValueError, "Invalid endianness specified in format.")
+
+  let originalPos: int = s.getPosition()
+
+  for i in countup(0, len(data)-1):
+    if fmt[i+1] == 'b':
+      s.write(int8(data[i]))
+    elif fmt[i+1] == 'B':
+      s.write(uint8(data[i]))
+    elif fmt[i+1] == '?':
+      s.write(bool(data[i]))
+    elif fmt[i+1] == 'h':
+      s.write(pack(int16(data[i]), endianness))
+    elif fmt[i+1] == 'H':
+      s.write(pack(uint16(data[i]), endianness))
+    elif fmt[i+1] == 'i':
+      s.write(pack(int32(data[i]), endianness))
+    elif fmt[i+1] == 'I':
+      s.write(pack(uint32(data[i]), endianness))
+    elif fmt[i+1] == 'q':
+      s.write(pack(int64(data[i]), endianness))
+    elif fmt[i+1] == 'Q':
+      s.write(pack(uint64(data[i]), endianness))
+    elif fmt[i+1] == 'f':
+      s.write((float32(data[i])))
+    elif fmt[i+1] == 'd':
+      s.write((float64(data[i])))
+
+  s.setPosition(originalPos)

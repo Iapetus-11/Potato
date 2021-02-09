f9b7bd04938446aadf2cf4031e86856730a471d7
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 16:09:09 -0500

    more testingggggggg

--- nimcraft.nim
+++ nimcraft.nim
@@ -1,7 +1,7 @@
 import src/je/util/struct
 
-var data: array[0..3, int] = [1, 2, 100000, 6969]
+var data: array[0..6, int] = [1, 2, 100000, 6969, 69, 420, 7]
 
-var result: string = struct.pack(">i?if", data)
+var result: string = struct.pack(">i?iIbBf", data)
 
 echo repr(result)

3095bcf73cb9a33c60ab9c15e33ee388cbf3b02b
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 18:02:23 -0500

    update test

--- nimcraft.nim
+++ nimcraft.nim
@@ -9,6 +9,8 @@ struct.pack(s, '<', 'i', 69)
 struct.pack(s, '>', 'f', 12.1)
 struct.pack(s, '>', 'q', 100000000)
 
+echo s.getPosition()
+
 echo struct.unpackUint8(s)
 echo struct.unpackInt32(s, '<')
 echo struct.unpackFloat32(s)

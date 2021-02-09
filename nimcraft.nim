85dbaa515fd6614c5e2b88801b33cb4ee87b9bce
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 16:36:49 -0500

    fix example/test

--- nimcraft.nim
+++ nimcraft.nim
@@ -1,7 +1,10 @@
+import streams
+
 import src/struct
 
+var s: Stream = newStringStream()
 var data: array[0..6, int] = [1, 2, 100000, 6969, 69, 420, 7]
 
-var result: string = struct.pack(">i?iIbBf", data)
+struct.pack(s, ">i?iIbBf", data)
 
-echo repr(result)
+echo repr(s.readAll())

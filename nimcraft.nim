82d27028ff136018897979696baa15f0e5b18096
Milo Weinberg <iapetus011@gmail.com>
2021-02-09 15:13:43 -0500

    fix usage of pack()

--- nimcraft.nim
+++ nimcraft.nim
@@ -1,8 +1,7 @@
 import src/je/util/struct
 
-var fmt: array[0..3, char] = ['b', 'B', 'q', 'i']
 var data: array[0..3, int] = [1, 2, 100000, 6969]
 
-var result: string = pack(fmt, data)
+var result: string = pack("bBqi", data)
 
 echo result

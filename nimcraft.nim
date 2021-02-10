b40a966835249dc899452fb37859c155ddfdc34d
Milo Weinberg <iapetus011@gmail.com>
2021-02-10 14:26:08 -0500

    add MUTF8 tests

--- nimcraft.nim
+++ nimcraft.nim
@@ -5,16 +5,21 @@ import src/je/structext
 import src/struct
 import src/mutf8
 
-var s: Stream = newStringStream()
+# Test MUTF8 utils
+let myString: string = "Hello World"
+echo mutf8.encodeMUTF8(myString)
+echo mutf8.decodeMUTF8(mutf8.encodeMUTF8(myString))
 
-struct.packUByte(s, 123)
-struct.packInt(s, 123, '<')
-struct.packFloat(s, 12.1)
-struct.packLong(s, 120000000)
-
-s.setPosition(0)
-
-echo struct.unpackUByte(s)
-echo struct.unpackInt(s, '<')
-echo struct.unpackFloat(s)
-echo struct.unpackLong(s)
+# var s: Stream = newStringStream()
+#
+# struct.packUByte(s, 123)
+# struct.packInt(s, 123, '<')
+# struct.packFloat(s, 12.1)
+# struct.packLong(s, 120000000)
+#
+# s.setPosition(0)
+#
+# echo struct.unpackUByte(s)
+# echo struct.unpackInt(s, '<')
+# echo struct.unpackFloat(s)
+# echo struct.unpackLong(s)
